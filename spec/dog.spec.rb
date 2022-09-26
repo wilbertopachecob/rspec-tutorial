76dbb4af2a79f035b4d5e9b8e88b075b50f2ad67
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-09-25 21:32:16 -0500

    using rspec predefined variable subject in test

--- spec/dog.spec.rb
+++ spec/dog.spec.rb
@@ -4,8 +4,7 @@ require './dog'
 describe Dog do
     describe '#bark' do
         it 'returns the string Woof!' do
-            dog = Dog.new
-            expect(dog.bark).to eql('Woof!')
+            expect(subject.bark).to eql('Woof!')
         end
     end
 end
