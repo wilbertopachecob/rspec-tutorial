1e131d145f2321faad6ca44ccb482af092eb9a5d
Wilberto Pacheco Batista <wbatista@doctorondemand.com>
2022-09-25 21:51:22 -0500

    added test for hungry method

--- dog.rb
+++ dog.rb
@@ -1,7 +1,14 @@
 class Dog
 
+    def initialize(hungry_level: 0)
+        @hungry_level = hungry_level
+    end
+
     def bark
         'Woof!'
     end
-    
+
+    def hungry?
+        @hungry_level > 5
+    end
 end
